fe96ddff2551e7acd9fa9ca45ea2b28900417e47
pink <133405253+pink-dev@users.noreply.github.com>
2023-12-03 16:43:56 +0000

    linux: updates and fixes to build system

--- make.sh
+++ make.sh
@@ -1,13 +1,13 @@
 export PINK_HOME="$(dirname "$(readlink -f "$0")")"/src
 export PINK_OS=linux
 
-if [ -e bin/pink ]; then
-  bin/pink -nosourcemap -notypeinfo -o src/c/out_pink_linux.c src/pink
+if [ -e dist/pink ]; then
+  dist/pink -nosourcemap -notypeinfo -o src/c/out_pink_linux.c src/pink
 else
   echo
   echo "-- No pink binary found; using previously built src/c/out_pink_linux.c --"
   echo
 fi
 
-gcc -static -o bin/pink src/c/pink.c
+gcc -static -o dist/pink src/c/pink.c
 
