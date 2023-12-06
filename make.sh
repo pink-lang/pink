0a860be05ca9cc832f6d998f0f352383446724c3
pink <133405253+pink-dev@users.noreply.github.com>
2023-12-06 12:40:15 +0100

    ci: fix build

--- make.sh
+++ make.sh
@@ -3,7 +3,7 @@ set -e
 export PINK_HOME="$(dirname "$(readlink -f "$0")")"/src
 export PINK_OS=linux
 
-dist/pink -nosourcemap -o src/c/out_pink_linux.c src/pink
+test -f ./dist/pink && dist/pink -nosourcemap -o src/c/out_pink_linux.c src/pink
 
 gcc -static -o dist/pink src/c/pink.c
 
